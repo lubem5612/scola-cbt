e68cdc0af965deba0f749de6bcb58d09500f8174
admin <enginlubem@ymail.com>
2023-05-24 17:12:32 +0100

    remove other packages

---
+++ src/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Transave\ScolaCbt\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasFactory, HasApiTokens;
+    protected $guarded = [
+        "id"
+    ];
+}
