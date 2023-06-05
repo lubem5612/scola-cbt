3d03652c02d2e8ad36f315fe587375fbf6e80a3b
Lubem Tser <enginlubem@ymail.com>
2023-06-05 16:35:35 +0100

    models and factories

---
+++ src/database/factories/StudentFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Transave\ScolaCbt\database\factories;
+
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Transave\ScolaCbt\Models\Student;
+use Transave\ScolaCbt\Models\User;
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+}
---
+++ tests/Unit/Models/StudentModelTest.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Transave\ScolaCbt\Tests\Unit\Models;
+
+
+use Orchestra\Testbench\TestCase;
+
+class StudentModelTest extends TestCase
+{
+
+}
