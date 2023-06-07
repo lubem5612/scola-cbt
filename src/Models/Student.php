60d783f6bca9bf16c41f38c227b50947d7f218aa
jonathanaudu007 <jaudu2@gmail.com>
2023-06-07 13:12:07 +0100

    feat: unit testing for user, student, questions and exam

--- src/Models/Student.php
+++ src/Models/Student.php
@@ -5,7 +5,7 @@ namespace Transave\ScolaCbt\Models;
 
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model
+use Illuminate\Database\Eloquent\Model;
 use Transave\ScolaCbt\Helpers\UUIDHelper;
 
 class Student extends Model
--- src/Models/User.php
+++ src/Models/User.php
@@ -3,20 +3,22 @@
 
 namespace Transave\ScolaCbt\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Transave\ScolaCbt\Helpers\UUIDHelper;
 
 class User extends Authenticatable
 {
-    use HasFactory, HasApiTokens, Notifiable, UUIDHelper;
+    use HasFactory, Notifiable, UUIDHelper;
     protected $table = "users";
 
-    protected $guarded = [
-        "id"
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'role'
     ];
 
     protected $hidden = [
