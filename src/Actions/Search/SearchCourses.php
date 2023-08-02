ab1b62324e2ba8b1af4321020d8617fffc16aee1
Lubem Tser <enginlubem@ymail.com>
2023-08-02 15:43:30 +0100

    search query

--- src/Actions/Search/SearchCourses.php
+++ src/Actions/Search/SearchCourses.php
@@ -10,17 +10,17 @@ class SearchCourses
 {
     use SearchHelper;
 
-    private function searchTerms()
+    public function searchTerms()
     {
         $search = $this->searchParam;
-        $this->queryBuilder->where(function ($query) use ($search) {
-            $query->where('name', 'like', "%$search")
-                ->orWhere('code', 'like', "%$search")
-                ->orWhere('credit_load', 'like', "%$search")
-                ->orWhereHas('department', function ($q) use ($search) {
-                    $q->where('name', 'like', "%$search")
-                        ->orWhere('id', 'like', "%$search")
-                        ->orWhereHas('faculty', function ($x) use ($search) {
+        $this->queryBuilder->where(function ($query) use($search) {
+            $query->where('name', 'like', "%$search%")
+                ->orWhere('code', 'like', "%$search%")
+                ->orWhere('credit_load', 'like', "%$search%")
+                ->orWhereHas('department', function ($q) use($search) {
+                    $q->where('name', 'like', "%$search%")
+                        ->orWhere('id', 'like', "%$search%")
+                        ->orWhereHas('faculty', function ($x) use($search) {
                             $x->where('name', 'like', "%$search%");
                         });
                 });
