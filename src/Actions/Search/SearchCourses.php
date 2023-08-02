c6fe453199eec73ed099d5430112717979a13178
Lubem Tser <enginlubem@ymail.com>
2023-08-02 15:01:46 +0100

    search query

--- src/Actions/Search/SearchCourses.php
+++ src/Actions/Search/SearchCourses.php
@@ -13,15 +13,13 @@ class SearchCourses
     private function searchTerms()
     {
         $search = $this->searchParam;
-        if (request()->query('department_id')) {
-            $this->queryBuilder->where('department_id', request()->query('department_id'));
-        }
         $this->queryBuilder->where(function ($query) use ($search) {
             $query->where('name', 'like', "%$search")
                 ->orWhere('code', 'like', "%$search")
                 ->orWhere('credit_load', 'like', "%$search")
                 ->orWhereHas('department', function ($q) use ($search) {
                     $q->where('name', 'like', "%$search")
+                        ->orWhere('id', 'like', "%$search")
                         ->orWhereHas('faculty', function ($x) use ($search) {
                             $x->where('name', 'like', "%$search%");
                         });
