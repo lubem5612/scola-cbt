72bed3c53fcf9615a0b10156dd8e33552cfa60f2
Lubem Tser <enginlubem@ymail.com>
2023-08-02 14:38:00 +0100

    add department id to courses

--- src/Actions/Search/SearchCourses.php
+++ src/Actions/Search/SearchCourses.php
@@ -13,17 +13,21 @@ class SearchCourses
     private function searchTerms()
     {
         $search = $this->searchParam;
-        $this->queryBuilder
-            ->where('name', 'like', "%$this->searchParam%")
-            ->orWhere('code', 'like', "%$this->searchParam%")
-            ->orWhere('department_id', 'like', "%$this->searchParam%")
-            ->orWhere('credit_load', 'like', "%$this->searchParam%")
-            ->orWhereHas('department', function ($q) use ($search) {
-                $q->where('name', 'like', "%$search%")
-                    ->orWhereHas('faculty', function ($q2) use ($search) {
-                        $q2->where('name', 'like', "%$search%");
-                    });
-            });
+        if (request()->query('department_id')) {
+            $this->queryBuilder->where('department_id', request()->query('department_id'));
+        }
+        $this->queryBuilder->where(function ($query) use ($search) {
+            $query->where('name', 'like', "%$search")
+                ->orWhere('code', 'like', "%$search")
+                ->orWhere('credit_load', 'like', "%$search")
+                ->orWhereHas('department', function ($q) use ($search) {
+                    $q->where('name', 'like', "%$search")
+                        ->orWhereHas('faculty', function ($x) use ($search) {
+                            $x->where('name', 'like', "%$search%");
+                        });
+                });
+        });
+
         return $this;
     }
 }
